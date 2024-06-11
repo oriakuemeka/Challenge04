0fb7728bfaf3a4dc1b2ce63bbfd17f10d75d83ac
oriakuemeka <ucintonet@gmail.com>
2024-06-11 18:39:33 -0500

    new file:   app/controllers/articles_controller.rb 	new file:   app/controllers/comments_controller.rb 	new file:   app/helpers/articles_helper.rb 	new file:   app/helpers/comments_helper.rb 	new file:   app/models/article.rb 	new file:   app/models/comment.rb 	new file:   app/models/concerns/visible.rb 	new file:   app/views/articles/_form.html.erb 	new file:   app/views/articles/edit.html.erb 	new file:   app/views/articles/index.html.erb 	new file:   app/views/articles/new.html.erb 	new file:   app/views/articles/show.html.erb 	new file:   app/views/comments/_comment.html.erb 	new file:   app/views/comments/_form.html.erb 	modified:   config/routes.rb 	new file:   db/migrate/20240611230059_create_articles.rb 	new file:   db/migrate/20240611230945_create_comments.rb 	new file:   db/migrate/20240611231819_add_status_to_articles.rb 	new file:   db/migrate/20240611231826_add_status_to_comments.rb 	new file:   db/schema.rb 	new file:   test/controllers/articles_controller_test.rb 	new file:   test/controllers/comments_controller_test.rb 	new file:   test/fixtures/articles.yml 	new file:   test/fixtures/comments.yml 	new file:   test/models/article_test.rb 	new file:   test/models/comment_test.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root "articles#index"
+
+  resources :articles do
+    resources :comments
+  end
+end
+
+
+Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
